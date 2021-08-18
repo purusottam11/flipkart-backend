806bdd8a13d3e0d4f1ba80a78b7cb442d97edd82
purusottam 2011 <ppurusottam1@gmail.com>
2021-08-18 15:23:22 +0530

    first commit

---
+++ src/main/java/com/purusottam/flipkartbackend/FlipkartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.purusottam.flipkartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipkartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipkartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/purusottam/flipkartbackend/FlipkartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purusottam.flipkartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipkartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
